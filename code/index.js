fd109d608c945eeb1cccba0a3b5c9bdddd8d2980
Soufian Tamim <soufiaintamim22@gmail.com>
2022-12-26 16:59:50 +0100

    initial commit

--- code/index.js
+++ code/index.js
@@ -1,5 +1,6 @@
 let myArray;
 
+
 function getData() {
   $.ajax({
     url: "index.json",
@@ -9,63 +10,110 @@ function getData() {
     },
   });
 }
+
 getData();
 
 
-function buildTable(data) {
-  let table = document.getElementById("myTable");
-  for (let i = 0; i < data.length; i++) {
-   let row =
-    `<tr>
-        <td>${data[i].film}</td>
-        <td>${data[i].director}</td>
-        <td>${data[i].runtime + " "}<spanp>min</spanp></td>
-        <td>${data[i].year}</td>
-        <td><img src="${data[i].poster}" alt="Poster"></td>
-         <td>
-            <li>${data[i].festivals[0]}</li>
-            <li>${data[i].festivals[1]}</li>
-           <li>${data[i].festivals[2]}</li>
-           <li>${data[i].festivals[3]}</li>
-          </td>
-          <td>
-           <li>${data[i].actors[0].firstName+" "+data[i].actors[0].lastName+" : "+data[i].actors[0].nationality}</li> 
-           <li>${data[i].actors[1].firstName+" "+data[i].actors[1].lastName+" : "+data[i].actors[1].nationality}</li> 
-           <li>${data[i].actors[2].firstName+" "+data[i].actors[2].lastName+" : "+data[i].actors[2].nationality}</li> 
-            <li>${data[i].actors[3].firstName+" "+data[i].actors[3].lastName+" : "+data[i].actors[3].nationality}</li> 
-          </td>
 
-     </tr>`;
-    table.innerHTML += row;
-  }
+
+
+function buildTable(data) {
+
+  let table = document.getElementById("myTable"); 
+
+  for (let i= 0; i < data.length; i++) {
+  console.log(i);
+    
+    
+     
+      let row =
+       `<tr>
+           <td>${data[i].film}</td>
+           <td>${data[i].director}</td>
+           <td>${data[i].runtime + " "}<spanp>min</spanp></td>
+           <td>${data[i].year}</td>
+             <td>
+               <img src="${data[i].poster}" alt="Poster">
+             </td> 
+             <td>
+               <li>${ data[i].festivals[0]} </li>
+             </td>
+             <td> 
+                <li>${data[i].actors[0].firstName+" "+data[0].actors[0].lastName +' '+ data[i].actors[0].nationality}</li> 
+              </td>
+        </tr>`;    
+        
+        
+        // for (let j = 0 ; j < myArray[0].festivals.length; j++){
+
+        //   row =`<tr>
+        //       <td>
+        //         <li>${ data[i].festivals[j]} </li>
+        //       </td>
+
+        //       <td> 
+        //         <li>${data[i].actors[j].firstName+" "+data[0].actors[0].lastName +' '+ data[i].actors[0].nationality}</li> 
+        //       </td>
+        //   </tr>`
+        
+        // }
+      
+        table.innerHTML += row;
+}
 }
 
 
-function stringSort(){
-  let table, rows, switching, i, x, y, shouldSwitch;
+
+
+
+
+
+// ===============================   sort string    ===============================================//
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
   table = document.getElementById("myTable");
   switching = true;
-  
+  dir = "asc"; 
+
   while (switching) {
+
     switching = false;
     rows = table.rows;
 
-    for (i = 1; i < rows.length - 1; i++) {
+    for (i = 1; i < (rows.length - 1); i++) {
+
       shouldSwitch = false;
 
-      x = rows[i].getElementsByTagName("td")[0];
-      y = rows[i + 1].getElementsByTagName("td")[0];
+      x = rows[i].getElementsByTagName("td")[n];
+      y = rows[i + 1].getElementsByTagName("td")[n];
 
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        shouldSwitch = true;
-        break;
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+
+          shouldSwitch = true;
+          break;
+        }
       }
     }
 
     if (shouldSwitch) {
+
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
+
+      switchcount ++;      
+    } else {
+
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
     }
   }
-
 }
