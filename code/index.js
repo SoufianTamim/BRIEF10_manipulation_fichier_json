e2abe0fb53aa8e944ff4c879827aa39a42a86fb6
Soufian Tamim <95128085+SoufianTamim@users.noreply.github.com>
2022-12-28 16:58:17 +0100

    Update index.js

--- code/index.js
+++ code/index.js
@@ -21,36 +21,37 @@ getData();  // self invoke the function that gets the data  and asign it to the 
 // build the function that builds the films data tables
 
 function buildTable(data) {
-	let table = document.getElementById("tBody"); // get the table id from html
+	let table = document.getElementById("tBody");
+	table.innerHTML = "";
+	// get the table id from html
 	for (let i = 0; i < data.length; i++) {
-		let row = "" ;
+		let row = "";
 
 		//first row build
 		row +=
-			`<tr class='film'>
-					 <td class='FilmName'>${data[i].film}</td>
+			`<tr>
+					 <td>${data[i].film}</td>
 					 <td>${data[i].director}</td>
-					 <td>${data[i].runtime + " "}<spanp>min</spanp></td>
+					 <td>${data[i].runtime}<span>min</span></td>
 					 <td>${data[i].year}</td>
 						 <td>
 							 <img src="${data[i].poster}" alt="Poster">
 						 </td>
-						 <td>  `  ; 		
 
-	for (let j = 0; j < myArray[i].festivals.length; j++) {
+						 <td>  `  ;
+
+		for (let j = 0; j < myArray[i].festivals.length; j++) {
 			row += ` <li>${data[i].festivals[j]} </li> `
 		};
 
-	row += 	`</td><td>`;
-		
+		row += `</td><td>`;
 
-	for (let j = 0; j < myArray[i].actors.length; j++) {
+		for (let j = 0; j < myArray[i].actors.length; j++) {
 
+			row += ` <li>${data[i].actors[j].firstName + " " + data[i].actors[j].lastName + ' : ' + data[i].actors[j].nationality}</li> `
+		};
 
-		row += ` <li>${data[i].actors[j].firstName + " " + data[i].actors[j].lastName + ' : ' + data[i].actors[j].nationality}</li> `
-	};
-
-	row += 	`</td></tr> `;
+		row += `</td></tr> `;
 
 		// creation of rows to apear in html
 
@@ -59,19 +60,32 @@ function buildTable(data) {
 }
 
 
+//===================================================   search function   =====================================================//
 
+const searchInput = document.getElementById("search");
 
+searchInput.addEventListener("keyup", function (event) {
+	const rows = document.querySelectorAll("tbody tr");
+
+	const q = event.target.value.toLowerCase();
+
+	rows.forEach((row) => {
+		row.querySelector("td").textContent.toLowerCase().startsWith(q)
+			? (row.style.display = "")
+			: (row.style.display = "none");
+	});
+});
 
 
 
 
 // ===============================   sort string    ===============================================//
-function SortTable(n) {
+const StringSort = (n) => {
 	let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
 	table = document.getElementById("myTable");
 	switching = true;
 	dir = "asc";
-	console.log(switchcount);
+
 
 	while (switching) {
 
@@ -82,8 +96,8 @@ function SortTable(n) {
 
 			shouldSwitch = false;
 
-			x = rows[i].getElementsByTagName("TD")[n];
-			y = rows[i + 1].getElementsByTagName("TD")[n];
+			x = rows[i].getElementsByTagName("td")[n];
+			y = rows[i + 1].getElementsByTagName("td")[n];
 
 			if (dir == "asc") {
 				if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
@@ -118,25 +132,38 @@ function SortTable(n) {
 
 // ====================================================   sort number   ========================================================//
 
-const NumberSort = () => {
-
-}
 
 
 
 
-//===================================================   search function   =====================================================//
 
-const searchInput = document.getElementById("search");
 
-searchInput.addEventListener("keyup", function (event) {
-const rows = document.querySelectorAll("tbody tr");
+const btn = document.getElementsByTagName("th")[2];
 
-	const q = event.target.value.toLowerCase();
+btn.addEventListener("click", function (event) {
 
-	rows.forEach((row) => {
-		row.querySelector("td").textContent.toLowerCase().startsWith(q)
-			? (row.style.display = "table-row")
-			: (row.style.display = "none");
+	myArray.sort(function (a, b) {
+
+		return a.runtime - b.runtime;
 	});
+	buildTable(myArray)
+
 });
+
+// const dir = document.getElementsByTagName("th")[1];
+
+// dir.addEventListener("click", function (event) {
+
+// 	myArray.sort(function (a, b) {
+// 		if ( ){
+
+// 			return a.runtime - b.runtime;
+// 		}else{
+// 			return b.runtime - a.runtime;
+// 		}
+
+// 	});
+
+// 	buildTable(myArray)
+
+// });
