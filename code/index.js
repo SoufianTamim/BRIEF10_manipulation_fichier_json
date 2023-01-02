07c13ad6998162433b93ea0203eb6c9d786fd616
Soufian Tamim <soufiaintamim22@gmail.com>
2023-01-02 12:20:39 +0100

    initial commit

--- code/index.js
+++ code/index.js
@@ -27,24 +27,28 @@ function buildTable(data) {
 			`<tr>
 					 <td>${data[i].film}</td>
 					 <td>${data[i].director}</td>
-					 <td>${data[i].runtime} <span> min</span></td>
+					 <td>${data[i].runtime}<span> min</span></td>
 					 <td>${data[i].year}</td>
 						 <td>
 							 <img src="${data[i].poster}" alt="Poster">
 						 </td>
 						 <td>  `  ;
-		for (let j = 0; j < myArray[i].festivals.length; j++) {
-		 row += ` <li>${data[i].festivals[j]}</li> `
-		};
-	 row += `</td><td>`;
-		for (let j = 0; j < myArray[i].actors.length; j++) {
-		 row += ` <li>${data[i].actors[j].firstName + " " + data[i].actors[j].lastName + ' : ' + data[i].actors[j].nationality}</li> `
-		};
-	 row += `</td></tr> `;
-		// creation of reaplaceds to apear in html
-		table.innerHTML += row;
+
+						for (let j = 0; j < myArray[i].festivals.length; j++) {
+						row += ` <li>${data[i].festivals[j]}</li> `
+						};
+
+						row += `</td><td>`;
+						for (let j = 0; j < myArray[i].actors.length; j++) {
+						row += ` <li>${data[i].actors[j].firstName + " " + data[i].actors[j].lastName + ' : ' + data[i].actors[j].nationality}</li> `
+						};
+
+						row += `</td></tr> `;
+						// creation of reaplaceds to apear in html
+						table.innerHTML += row;
 	}
 }
+
 
 //===================================================   search function   =====================================================//
 
@@ -68,25 +72,25 @@ const StringSort = (n) => {
 	let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
 	table = document.getElementById("myTable");
 	switching = true;
-// Set the sorting direction to ascending:
+	// Set the sorting direction to ascending:
 	dir = "asc";
 	/* Make a loop that will continue until
-no switching has been done: */
+	no switching has been done: */
 	while (switching) {
-	// Start by saying: no switching is done:
+		// Start by saying: no switching is done:
 		switching = false;
 		rows = table.rows;
 		/* Loop through all table rows (except the
-	first, which contains table headers): */
+		first, which contains table headers): */
 		for (i = 1; i < (rows.length - 1); i++) {
-		// Start by saying there should be no switching:
+			// Start by saying there should be no switching:
 			shouldSwitch = false;
 			/* Get the two elements you want to compare,
-		one from current row and one from the next: */
+			one from current row and one from the next: */
 			x = rows[i].getElementsByTagName("td")[n];
 			y = rows[i + 1].getElementsByTagName("td")[n];
 			/* Check if the two rows should switch place,
-		based on the direction, asc or desc: */
+			based on the direction, asc or desc: */
 			if (dir == "asc") {
 				if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
 				// If so, mark as a switch and break the loop:
@@ -98,8 +102,6 @@ no switching has been done: */
 				if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
 				// If so, mark as a switch and break the loop:
 
-
-				
 					shouldSwitch = true;
 					break;
 				}
